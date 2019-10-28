843af12f669790bac240c3436fa7168e1619a75b
Zipexpo <nvtngan@hcmus.edu.vn>
2019-10-28 12:30:16 -0500

    update

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,6 +1,7 @@
 let height = window.innerHeight;
 let mainheight;
 $(function () {
+    // grid
     mainheight = $('#overlay_panel').innerHeight();
     d3.select('.menu-open').on('change',function(){
         d3.select('.menu').classed('active',$(this).prop('checked'));
@@ -25,4 +26,26 @@ $(function () {
     $('#right_panel').gridstack(_.defaults({
         float: true
     }, options));
+
+    // color panel -- https://www.cssscript.com/color-picker-pro/
+    var mirror_light_color = new ColorPicker.TabPalette('#lightControl', {
+        color: '#009de7',
+        placement: 'left',
+        anchor: {
+            cssProperty: 'background'
+        },
+        history: {
+            hidden: false, //  shows or hides history block
+            colors: [] // ['red', 'green', 'rgba(255, 1, 128, 1)']
+        }
+        });
+    mirror_light_color.on('change', function(color) {
+        mainContent.style.setProperty('--light-color',color.hex)
+    });
+
+    // $('.colorpicker-theme').on('change', function () {
+    //     var val = $(this).val();
+    //     var style = '<link id="colorpicker-style" rel="stylesheet" href="' + val + '">';
+    //     $('head').append(style);
+    // });
 });
